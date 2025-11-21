22ea6afcaab19171d4027754f033ff06c92b7359
TheConnMan <bcconn2112@gmail.com>
2025-11-21 06:36:33 -0500

    Symlink in .bashrc

--- user-data.sh
+++ user-data.sh
@@ -52,6 +52,7 @@ ln -sf /workspace/git "$HOME_DIR/git"
 ln -sf /workspace/.aws "$HOME_DIR/.aws"
 rm -rf "$HOME_DIR/.ssh"
 ln -sf /workspace/.ssh "$HOME_DIR/.ssh"
+ln -sf /workspace/.bashrc "$HOME_DIR/.bashrc"
 
 # Install and configure Tailscale
 echo "Setting up Tailscale..."
@@ -64,9 +65,5 @@ fi
 tailscale up --authkey=tskey-auth-ko1HUif8m511CNTRL-iFWseP26cxLK2Fb5WC3twL19gsaZ4HEFe --hostname=remote-dev
 
 echo "Tailscale configured and started"
-
-# Add to .bashrc
-echo "export PATH=\"/workspace/poetry/bin:/workspace/bin:\$PATH\"" >> "$HOME_DIR/.bashrc"
-echo 'parse_git_branch() { git branch 2>/dev/null | sed -n "s/^* //p"; }; export PS1="\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \[\e[1;33m\](\$(parse_git_branch))\[\e[0m\]\$ "' >> "$HOME_DIR/.bashrc"
 
 echo "=== User-data script completed at $(date) ==="
